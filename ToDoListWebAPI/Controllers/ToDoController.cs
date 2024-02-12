bb8b6d0949471475b6edc5b1e787d75ce55a8240
saurav dahal <d.saurab1@gmail.com>
2024-02-11 22:00:52 -0500

    feat: added get method that lists all task for which completed date has not been set

--- ToDoListWebAPI/Controllers/ToDoController.cs
+++ ToDoListWebAPI/Controllers/ToDoController.cs
@@ -7,6 +7,19 @@ namespace ToDoListWebAPI.Controllers
 {
     public class ToDoController : Controller
     {
-        
+        private readonly ToDoDbContext todocontext;
+
+        public ToDoController(ToDoDbContext context)
+        {
+            this.todocontext = context;
+        }
+        [HttpGet("/")]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetToDoItems()
+        {
+            // Retrieve ToDoItems with no CompletedDate set
+            var toDoItems = await todocontext.ToDoItems.Where(item => item.CompletedDate == null).ToListAsync();
+
+            return Ok(toDoItems);
+        }
     }
 }
