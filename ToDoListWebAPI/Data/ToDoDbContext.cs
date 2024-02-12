aab324a4af80b402b78713aff1601d6dcf69c3f5
saurav dahal <d.saurab1@gmail.com>
2024-02-11 21:59:59 -0500

    feat: added database file

--- ToDoListWebAPI/Data/ToDoDbContext.cs
+++ ToDoListWebAPI/Data/ToDoDbContext.cs
@@ -5,7 +5,14 @@ namespace ToDoListWebAPI.Data
 {
     public class ToDoDbContext : DbContext
     {
-        
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+        public ToDoDbContext() { }
+
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options) => Database.Migrate();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite();
     }
 
 }
